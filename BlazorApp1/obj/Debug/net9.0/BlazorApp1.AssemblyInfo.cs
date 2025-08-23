3acab4178a9690db3ef84be614e50d28dfafa136
Edgar Rivero <riveroedgar.jm@gmail.com>
2025-08-23 15:46:19 -0500

    fix: clean credentials

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd02e0e20340fc6843fa1ea5affc06319368e8b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e910a0c385bc35f854f2166fde2a1f47c9af4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
